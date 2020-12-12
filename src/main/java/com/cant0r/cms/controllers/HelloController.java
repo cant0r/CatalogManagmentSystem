975b5c23837cf8a3b0e84d246c6ec8b365c44c50
Racs Tamás <cant0r.universal@gmail.com>
2020-12-12 15:53:29 +0100

    Basic things do their basic things. Yay!

--- src/main/java/com/cant0r/cms/controllers/HelloController.java
+++ src/main/java/com/cant0r/cms/controllers/HelloController.java
@@ -1,2 +1,13 @@
-package com.cant0r.cms.controllers;public class HelloController {
+package com.cant0r.cms.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greetClient() {
+        return "Hello, World! - said the server";
+    }
 }
--- src/main/java/com/cant0r/cms/security/CMSSecurity.java
+++ src/main/java/com/cant0r/cms/security/CMSSecurity.java
@@ -1,2 +1,17 @@
-package com.cant0r.cms.security;public class CMSSecurity {
+package com.cant0r.cms.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class CMSSecurity extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/hello")
+                    .permitAll();
+    }
 }
