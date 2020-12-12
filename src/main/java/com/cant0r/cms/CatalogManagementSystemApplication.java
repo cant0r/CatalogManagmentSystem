213d3c5f84e2ae64b2265235fe894ae897fea6ef
Racs Tamás <cant0r.universal@gmail.com>
2020-12-12 14:52:15 +0100

    Including junk of spring init

---
+++ src/main/java/com/cant0r/cms/CatalogManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cant0r.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cant0r/cms/CatalogManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cant0r.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
