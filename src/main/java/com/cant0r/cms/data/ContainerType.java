b60be0df47f707c896887e061519aef79f8fb60c
Racs Tamás <cant0r.universal@gmail.com>
2020-12-13 16:24:29 +0100

    Added models for the db relations

---
+++ src/main/java/com/cant0r/cms/data/ContainerType.java
@@ -0,0 +1,19 @@
+package com.cant0r.cms.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+public class ContainerType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String name;
+    private String description;
+}
