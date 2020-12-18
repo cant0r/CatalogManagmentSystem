5365915b5ae2544128ce36cef114277832180541
Racs Tamás <cant0r.universal@gmail.com>
2020-12-18 20:30:55 +0100

    Added a new API endpoint to provide login related abilities to users

--- src/main/java/com/cant0r/cms/controllers/LoginController.java
+++ src/main/java/com/cant0r/cms/controllers/LoginController.java
@@ -1,12 +1,21 @@
 package com.cant0r.cms.controllers;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cant0r.cms.security.User;
+import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.Base64;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/api/login")
 public class LoginController {
+    @PostMapping
+    public boolean login(@RequestBody User user) {
+        return (user.getUsername().equals("cms") && user.getPassword().equals("cms")) ||
+                (user.getUsername().equals("admin") && user.getPassword().equals("admin"));
+    }
 }
+
