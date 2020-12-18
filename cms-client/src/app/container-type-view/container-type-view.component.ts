7eb858527dd0abd24347517c8d7f45b678b21edd
Racs Tamás <cant0r.universal@gmail.com>
2020-12-18 20:28:23 +0100

    Implemented authentication in the various API calling methods. Also added logout funcionality to the component

--- cms-client/src/app/container-type-view/container-type-view.component.ts
+++ cms-client/src/app/container-type-view/container-type-view.component.ts
@@ -1,6 +1,8 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-container-type-view',
@@ -12,29 +14,39 @@ export class ContainerTypeViewComponent implements OnInit {
   badResponse = false;
   saveSuccess = false;
 
-  name:string = "";
-  description:string = "";
+  name: string = "";
+  description: string = "";
 
-  page:number = 0;
-  entry:number = 5;
+  page: number = 0;
+  entry: number = 5;
 
+  headers: HttpHeaders;
 
   containerTypes: ContainerType[];
   containerTypeAPI: string;
 
   constructor(private router: Router,
-              private httpClient: HttpClient) {
-        this.containerTypeAPI = "http://localhost:8080/api/types"}
+    private httpClient: HttpClient) {
+
+    this.containerTypeAPI = "http://localhost:8080/api/types";
+
+    this.headers = new HttpHeaders({
+      'Authorization': 'Basic ' + sessionStorage.getItem('token')
+    });
+
+
+  }
 
   ngOnInit(): void {
     this.getContainerTypes(0);
   }
   getContainerTypes(page: number) {
     var params = new HttpParams()
-                      .set("pageNumber", page.toString())
-                      .set("entries", this.entry.toString());
-     this.httpClient.get<ContainerType[]>(this.containerTypeAPI + "/all",
-      {params : params}).subscribe( (response) => {
+      .set("pageNumber", page.toString())
+      .set("entries", this.entry.toString());
+    this.httpClient.get<ContainerType[]>(this.containerTypeAPI + "/all",
+      { params: params, headers: this.headers })
+      .subscribe((response) => {
         this.containerTypes = response;
       });
   }
@@ -44,35 +56,50 @@ export class ContainerTypeViewComponent implements OnInit {
   }
 
   onSaveClick() {
-    for(let item of this.containerTypes) {
-      if(item.name != "")
-        this.httpClient.post<ContainerType>(this.containerTypeAPI, item).subscribe((response) => {
-          console.log(response);
-      })
+    for (let item of this.containerTypes) {
+      if (item.name != "")
+        this.httpClient.post<ContainerType>(this.containerTypeAPI, item, { headers: this.headers})
+          .pipe(catchError((err): Observable<ContainerType[]> => {
+            this.badResponse = true;
+            return null;
+          }))
+          .subscribe((response) => {
+            console.log(response);
+            if (!this.badResponse) {
+              this.saveSuccess = true;
+              setTimeout(() => {
+                this.saveSuccess = false;
+              }, 1500);
+            }
+          })
       else
         this.badResponse = true;
     };
-    this.saveSuccess = true;
-    setTimeout(() => {
-      this.saveSuccess = false;
-    }, 1500);
+
+
   }
 
   onDangerClick(id: number) {
-    this.httpClient.delete(this.containerTypeAPI + "/" + this.containerTypes[id].name)
+    this.httpClient.delete(this.containerTypeAPI + "/" + this.containerTypes[id].name, { headers: this.headers})
+      .pipe(catchError((err): Observable<ContainerType[]> => {
+        this.badResponse = true;
+        return null;
+      }))
       .subscribe((response) => {
         console.log(response);
+
+        if (!this.badResponse)
+          this.containerTypes.splice(id % this.containerTypes.length, 1);
       });
-    this.containerTypes.splice(id % this.containerTypes.length, 1);       
   }
   onSearchClick() {
     var ps = new HttpParams();
-    ps = ps.append("n",this.name);
+    ps = ps.append("n", this.name);
     ps = ps.append("d", this.description);
     ps = ps.append("pageNumber", this.page.toString());
     ps = ps.append("entries", this.entry.toString());
 
-    this.httpClient.get<ContainerType[]>(this.containerTypeAPI + "/all-params", {params : ps})
+    this.httpClient.get<ContainerType[]>(this.containerTypeAPI + "/all-params", { params: ps, headers: this.headers })
       .subscribe((response) => {
         this.containerTypes = response;
       });
@@ -81,12 +108,11 @@ export class ContainerTypeViewComponent implements OnInit {
   onPreviousClick() {
     this.page -= 1;
 
-    if(this.page < 0)
+    if (this.page < 0)
       this.page = 0;
 
     this.getContainerTypes(this.page);
-    if(this.containerTypes.length == 0)
-    {
+    if (this.containerTypes.length == 0) {
       this.page += 1;
       this.getContainerTypes(this.page);
     }
@@ -96,11 +122,14 @@ export class ContainerTypeViewComponent implements OnInit {
     this.page += 1;
 
     this.getContainerTypes(this.page);
-    if(this.containerTypes.length == 0)
-    {
+    if (this.containerTypes.length == 0) {
       this.page = 0;
       this.getContainerTypes(this.page);
     }
+  }
+
+  logOut() {
+    sessionStorage.setItem("token", "");
   }
 }
 
