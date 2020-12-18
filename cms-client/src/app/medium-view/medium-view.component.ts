15da531047cc8cda3faa8ee157a776038d4fe05c
Racs Tamás <cant0r.universal@gmail.com>
2020-12-18 20:29:15 +0100

    Implemented authentication in the various API calling methods in the Medium component. Also added logout funcionality to the component.

--- cms-client/src/app/medium-view/medium-view.component.ts
+++ cms-client/src/app/medium-view/medium-view.component.ts
@@ -1,6 +1,6 @@
 import { Container } from './../container-view/container-view.component';
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs';
@@ -15,20 +15,25 @@ export class MediumViewComponent implements OnInit {
   badResponse = false;
   saveSuccess = false;
 
-  page:number = 0;
-  entry:number = 5;
+  page: number = 0;
+  entry: number = 5;
 
   capacity: number;
   name: string;
   container_id: number;
 
+  headers: HttpHeaders;
+
   containerAPI: string;
   mediums: Medium[];
 
   constructor(private router: Router,
-              private httpClient: HttpClient) { 
-                  this.containerAPI = "http://localhost:8080/api/mediums"
-              }
+    private httpClient: HttpClient) {
+    this.containerAPI = "http://localhost:8080/api/mediums";
+    this.headers = new HttpHeaders({
+      'Authorization': 'Basic ' + sessionStorage.getItem('token')
+    });
+  }
 
   ngOnInit(): void {
     this.getMediums(0);
@@ -36,12 +41,17 @@ export class MediumViewComponent implements OnInit {
 
   getMediums(page: number) {
     var params = new HttpParams()
-        .set("pageNumber", page.toString())
-        .set("entries", this.entry.toString());
+      .set("pageNumber", page.toString())
+      .set("entries", this.entry.toString());
     this.httpClient.get<Medium[]>(this.containerAPI + "/all",
-    {params : params}).subscribe( (response) => {
-    this.mediums = response
-});
+      { params: params, headers: this.headers })
+      .pipe(catchError((e): Observable<Medium[]> => {
+        this.router.navigateByUrl("/login");
+        return null;
+      }))
+      .subscribe((response) => {
+        this.mediums = response
+      });
   }
 
   onNewClick() {
@@ -51,56 +61,63 @@ export class MediumViewComponent implements OnInit {
   }
 
   onSaveClick() {
-    for(let item of this.mediums) {
-      this.httpClient.post<Medium>(this.containerAPI, item)
+    for (let item of this.mediums) {
+      this.httpClient.post<Medium>(this.containerAPI, item, { headers: this.headers})
         .pipe(catchError(
           (err): Observable<Medium[]> => {
-              this.badResponse = true;
-              return null;
+            this.badResponse = true;
+            return null;
           }
         ))
         .subscribe((response) => {
           console.log(response);
+          if(!this.badResponse) {
+            this.saveSuccess = true;
+        
+            setTimeout(() => {
+              this.saveSuccess = false;
+            }, 1500);
+          }
         })
     };
-
-    this.saveSuccess = true;
-
-    setTimeout(() => {
-      this.saveSuccess = false;
-    }, 1500);
   }
 
   onDangerClick(id: number) {
-    this.httpClient.delete(this.containerAPI + "/" + this.mediums[id].id)
-    .subscribe((response) => {
-      console.log(response);
-    });
-    this.mediums.splice(id % this.mediums.length, 1);
+    this.httpClient.delete(this.containerAPI + "/" + this.mediums[id].id, { headers: this.headers})
+      .pipe(catchError(
+        (err): Observable<Medium[]> => {
+          this.badResponse = true;
+          return null;
+        }
+      ))
+      .subscribe((response) => {
+        console.log(response);
+        if (!this.badResponse)
+          this.mediums.splice(id % this.mediums.length, 1);
+      });
   }
   onSearchClick() {
     var ps = new HttpParams();
-    ps = ps.append("n",this.name);
+    ps = ps.append("n", this.name);
     ps = ps.append("c", this.capacity.toString());
     ps = ps.append("pageNumber", this.page.toString());
     ps = ps.append("entries", this.entry.toString());
 
-    this.httpClient.get<Medium[]>(this.containerAPI + "/all-params", {params : ps})
+    this.httpClient.get<Medium[]>(this.containerAPI + "/all-params", { params: ps, headers: this.headers })
       .subscribe((response) => {
         this.mediums = response;
-        
+
       });
   }
 
   onPreviousClick() {
     this.page -= 1;
 
-    if(this.page < 0)
+    if (this.page < 0)
       this.page = 0;
 
     this.getMediums(this.page);
-    if(this.mediums.length == 0)
-    {
+    if (this.mediums.length == 0) {
       this.page += 1;
       this.getMediums(this.page);
     }
@@ -110,13 +127,15 @@ export class MediumViewComponent implements OnInit {
     this.page += 1;
 
     this.getMediums(this.page);
-    if(this.mediums.length == 0)
-    {
+    if (this.mediums.length == 0) {
       this.page = 0;
       this.getMediums(this.page);
     }
   }
 
+  logOut() {
+    sessionStorage.setItem("token", "");
+  }
 }
 
 export class Medium {
