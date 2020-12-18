f630ac96fb651e50f19eef904c0f12cf85186c21
Racs Tamás <cant0r.universal@gmail.com>
2020-12-18 22:02:11 +0100

    Added configuration to deploy the webapp to tomcat

--- src/main/java/com/cant0r/cms/CatalogManagementSystemApplication.java
+++ src/main/java/com/cant0r/cms/CatalogManagementSystemApplication.java
@@ -3,12 +3,13 @@ package com.cant0r.cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.cant0r.cms.data")
 @EntityScan("com.cant0r.cms.data")
-public class CatalogManagementSystemApplication {
+public class CatalogManagementSystemApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogManagementSystemApplication.class, args);
